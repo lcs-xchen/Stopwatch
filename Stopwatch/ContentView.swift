c87f193a54c29a84275fbe0ebf6501f52c97aca4
Cindy Chen <xchen@lcs.on.ca>
2023-11-02 09:43:41 -0400

    I have added the gray button underneath the "00:09.96"

--- Stopwatch/ContentView.swift
+++ Stopwatch/ContentView.swift
@@ -20,6 +20,24 @@ struct ContentView: View {
                 Text ("00:09.96")
                     .font(Font.system(size: 90, weight:.thin))
                     .foregroundColor(.white)
+                
+                ZStack {
+                    //First layer
+                    Circle()
+                        .foregroundColor(.gray)
+                    .frame(width: 100)
+                    
+                    //Second layer
+                    Circle()
+                        .foregroundColor(.black)
+                        .frame(width: 93)
+                    
+                    //Third layer
+                    Circle()
+                        .foregroundColor(.gray)
+                        .frame(width: 89)
+                    
+                }
             }
             .padding()
         }
