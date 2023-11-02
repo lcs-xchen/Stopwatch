8779d6e73dd42b45dec56873b5631e5f07af0e0c
Cindy Chen <xchen@lcs.on.ca>
2023-11-02 09:21:02 -0400

    Initial Commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Cindy Chen on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
